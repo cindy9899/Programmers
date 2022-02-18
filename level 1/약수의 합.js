4f158aa947cdcb1fc5af83da1ffdc5a5dea7abd7
cindy9899 <yeonhoo9899@gmail.com>
2022-02-19 02:15:37 +0900

    add measure

---
+++ level 1/약수의 합.js
@@ -0,0 +1,18 @@
+/*
+정수 n을 입력받아 n의 약수를 모두 더한 값을 리턴
+
+<제한사항>
+1. n은 0 이상 3000 이하인 정수입니다.
+
+n 12
+return 28 (1+2+3+4+6+12)
+*/
+
+function solution(n) {
+    var answer = 0;
+    for(let i=1;i<=n;i++){
+        if(n%i==0)
+            answer+=i;
+    }
+    return answer;
+}
