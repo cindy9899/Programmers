0d8b9bfd5d0f78bc786859912e74bb50cf9f619c
cindy9899 <yeonhoo9899@gmail.com>
2022-03-02 21:28:55 +0900

    k numbers push

--- level 1/K번째 수.js
+++ level 1/K번째 수.js
@@ -6,6 +6,18 @@ array, i=(int), j=(int), k=(int)
 <제한사항>
 1. array의 길이는 1 이상 100 이하입니다.
 2. array의 각 원소는 1 이상 100 이하입니다.
+3. commands의 길이는 1 이상 50 이하입니다.
+4. commands의 각 원소는 길이가 3 입니다.
 
+array [1, 5, 2, 6, 4, 7, 4]
+commands [[2,5,3], [4,4,1], [1,7,3]]
+return => [5,6,3]
 */
 
+function solution(array, commands) {
+    var answer = [];    
+    for(let i=0; i<commands.length; i++){
+        answer.push(array.slice(commands[i][0]-1, commands[i][1]).sort(function(a,b){return a-b})[commands[i][2]-1])
+    }
+    return answer;
+}
