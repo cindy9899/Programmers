cccc3acb946aac93b6f1acf1b31f8bf7d9ffb4e6
cindy9899 <yeonhoo9899@gmail.com>
2022-03-04 21:22:23 +0900

    caesor

--- level 1/ㄱ시저 암호.js
+++ level 1/ㄱ시저 암호.js
@@ -7,3 +7,30 @@
 3. s의 길이는 8000이하입니다.
 4. n은 1 이상, 25 이하인 자연수입니다.
 */
+
+function solution(s, n) {
+    var answer = '';
+    for(let i=0; i<s.length; i++){
+        var letter=s.charCodeAt(i)
+        if(letter==32){
+            answer+=" "
+            continue;
+        }
+        else if(65<=letter && letter<=90){
+            letter+=n
+            if(letter>90){
+                letter-=26
+            }
+        }
+        else{
+            letter+=n
+            if(letter>122){
+                letter-=26
+            }
+        }
+        letter=String.fromCharCode(letter)
+        answer+=letter
+    }
+    
+    return answer;
+}
