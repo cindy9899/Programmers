d0e426e9b1edc5341a437f3f203e97e6f6ebbe8d
박연후 <cindy@bag-yeonhuui-MacBook-Pro.local>
2022-03-09 14:49:30 +0900

    cellphone

--- level 1/핸드폰 번호 가리기.js
+++ level 1/핸드폰 번호 가리기.js
@@ -8,3 +8,12 @@
 phone_number 01033334444
 return *******4444
 */
+
+function solution(phone_number) {
+    var answer = '';
+    for(let i=0;i<phone_number.length-4;i++)
+        answer+='*';
+    for(let i=phone_number.length-4;i<phone_number.length;i++)
+        answer+=phone_number[i];
+    return answer;
+}
