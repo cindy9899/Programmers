6ecdc0b38d8b7ffc33601f8cff7744953c952ac4
yeonhoo <yeonhoo9899@gmail.com>
2022-03-28 21:31:22 +0900

    square

--- level 2/멀쩡한 사각형.js
+++ level 2/멀쩡한 사각형.js
@@ -12,3 +12,11 @@ H 12
 return => 80
 */
 
+function solution(w, h) {
+    var answer = 0;
+    const [a,b]=[Math.min(w,h), Math.max(w,h)]
+    for(let i=1; i<b; i++){
+        answer+=Math.floor(a/b*i)
+    }
+    return answer*2;
+}
