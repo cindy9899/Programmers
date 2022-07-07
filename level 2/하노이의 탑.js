b8ba5994b34906ac80ed706f077b91d479ff19d3
cindy9899 <yeonhoo9899@gmail.com>
2022-07-07 14:27:07 +0900

    Hanoi

--- level 2/하노이의 탑.js
+++ level 2/하노이의 탑.js
@@ -13,3 +13,21 @@ n 2
 return => [[1, 2], [1, 3], 2, 3]
 */
 
+function solution(n) {
+    var answer = [];
+    
+    function Hanoi(num, from, other, to){
+        if(num===0){
+            return;
+        }else{
+            //받아온 원반 갯수보다 하나 적은 원반들을 목적지가 아닌 다른 곳으로
+            Hanoi(num-1, from, to, other);
+            answer.push([from, to]);
+            //다른곳으로 옮겼던 원반들을 그 위로
+            Hanoi(num-1, other, from, to);
+        }
+    }
+    Hanoi(n, 1, 2, 3);
+    
+    return answer;
+}
